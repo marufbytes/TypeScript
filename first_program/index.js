f6fb0896833475406be8ba7828ca9182326044eb
marufbytes <marufahammed215@gmail.com>
2026-06-24 02:08:23 +0600

    Ts Basic

---
+++ first_program/index.js
@@ -0,0 +1,12 @@
+"use strict";
+let userName = "Maruf Ahammed";
+let age = 25;
+let country = "Bangaldesh";
+console.log(userName);
+console.log(age);
+console.log(country);
+//tsc index.ts  -> index.js  -> node index.js
+function addNumber(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumber(20, 30);
